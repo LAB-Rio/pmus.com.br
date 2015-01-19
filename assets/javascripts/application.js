75569d06455a78df143c946eb366d4e0cd4f425f
Luiz Fonseca <eu@luiz.cc>
2015-01-19 20:31:18 -0200

    Add map lens

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -8,7 +8,7 @@ window.Slider = {
   initialize: function(){
     this.watchArrowClick();
     this.interval = setInterval(Slider.startSlide, 8000);
-
+    this.startLens();
 
     $('a.learn-more').on('click', function(){
       $('#plan .about').slideDown();
@@ -16,6 +16,22 @@ window.Slider = {
     });
   },
 
+
+  startLens: function(){
+    $("#map_image").mlens(
+      {
+        imgSrc: $("#map_image").data("big"),	    // path of the hi-res version of the image
+        lensShape: "square",				// shape of the lens (circle/square)
+        lensSize: 350,					// size of the lens (in px)
+        borderSize: 2,					// size of the lens border (in px)
+        borderColor: "#fff",				// color of the lens border (#hex)
+        borderRadius: 0,				// border radius (optional, only if the shape is square)
+        imgOverlay: $("#green_monster").attr("data-overlay"), // path of the overlay image (optional)
+        overlayAdapt: true, // true if the overlay image has to adapt to the lens size (true/false)
+        zoomLevel: 1                                    // zoom level multiplicator (number)
+      });
+
+  },
 
   watchArrowClick: function(){
     var self = this;
@@ -90,3 +106,6 @@ window.Slider = {
 };
 
 Slider.initialize();
+
+
+
